f6bdc4f85e817026d2093338c59fb4e90abeb041
fbn <fabiangomes2015@gmail.com>
2023-03-28 10:05:07 -0500

    Tarea JEJEJEEJ

---
+++ app/src/main/java/com/example/datos/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.datos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText Opcion;
+
+    EditText resultadoOpcion;
+
+    Button Test;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Opcion = (EditText) findViewById(R.id.num1);
+
+        resultadoOpcion = (EditText) findViewById(R.id.Operation);
+
+        Test = (Button) findViewById(R.id.test);
+        Test.setOnClickListener(this);
+    }
+
+    //Metodo Boton El boton siguiente
+
+   /* public void Siguiente(View view){
+        Intent siguiente = new Intent(this,Resultado.class);
+        startActivity(siguiente);
+
+    }
+*/
+
+
+
+    @Override
+    public void onClick(View view) {
+        float n1;
+
+
+        n1 = Float.parseFloat(Opcion.getText().toString());
+
+
+        if (n1 == 1.0){
+            Intent siguiente = new Intent(this,Resultado.class);
+            startActivity(siguiente);
+        }else if( n1 == 2.0){
+            Intent resta = new Intent(this,Resta.class);
+            startActivity(resta);
+        }else if ( n1 == 3.0){
+            Intent multiplicacion = new Intent(this, Multiplicacion.class);
+            startActivity(multiplicacion);
+        } else if (n1 == 4.0) {
+            Intent dividir = new Intent(this, Division.class);
+            startActivity(dividir);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/datos/Multiplicacion.java
@@ -0,0 +1,58 @@
+package com.example.datos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Multiplicacion extends AppCompatActivity implements View.OnClickListener {
+
+    EditText num1,num2,Resultado2;
+
+    Button Operar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        num1 = (EditText)findViewById(R.id.num1);
+        num2 = (EditText)findViewById(R.id.num2);
+
+        Resultado2 = (EditText) findViewById(R.id.resultado);
+
+        Operar = (Button) findViewById(R.id.Action);
+        Operar.setOnClickListener(this);
+
+
+
+
+    }
+    //metodo para regresar
+    public void Anterior(View view){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        float n1,res;
+        float n2;
+
+        n1 = Float.parseFloat(num1.getText().toString());
+        n2 = Float.parseFloat(num2.getText().toString());
+
+        switch (view.getId()){
+            case  R.id.Action:
+                res = n1*n2;
+                Resultado2.setText("El Resultado es: "+ res);
+        }
+    }
+}
